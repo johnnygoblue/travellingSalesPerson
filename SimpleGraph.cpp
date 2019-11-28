e47c87e92df45c4a8b47c3dd106414243ac712db
Johnny Chan <chanyao@umich.edu>
2019-11-27 23:33:45 -0500

    Add SimpleGraph

---
+++ SimpleGraph.cpp
@@ -0,0 +1,76 @@
+// Project Identifier: 5949F553E20B650AB0FB2266D3C0822B13D248B0
+
+#include <algorithm>
+#include <limits>
+#include <cmath>
+#include <cassert>
+#include <list>
+
+#include "SimpleGraph.h"
+
+using namespace std;
+
+SimpleGraph::VertType SimpleGraph::Vertex::get_vert_type() const {
+	if (x < 0 && y < 0) return SimpleGraph::VertType::WATER;
+	if (x > 0 || y > 0) return SimpleGraph::VertType::LAND;
+	return SimpleGraph::VertType::COAST;
+}
+
+SimpleGraph::SimpleGraph(istream &is, bool border_in) : border(border_in) {
+	size_t size_in;
+	is >> size_in;
+	vertices.reserve(size_in);
+
+	int x_in, y_in;
+	while (is >> x_in >> y_in) vertices.push_back(
+		{
+			numeric_limits<double>::infinity(),
+			x_in, y_in,
+			0,
+			false,
+		}
+	);
+}
+
+size_t SimpleGraph::total_size() const { return vertices.size(); }
+
+bool SimpleGraph::is_valid_vertex(const Vertex &v) { return !v.deleted; }
+
+double SimpleGraph::dist(const Vertex &v_1, const Vertex &v_2) const {
+
+	if (&v_1 == &v_2) return 0;
+	/*
+	Here's a neat trick:
+		LAND + LAND = 0
+		LAND + WATER = 1
+		LAND + COAST = 2
+		WATER + WATER = 2
+		WATER + COAST = 3
+		COAST + COAST = 4
+	If we get a sume of 1, it's infinity. Otherwise, use Euclidean distance.
+	*/
+	if (border && ((int)v_1.get_vert_type() + (int)v_2.get_vert_type() == 1))
+		return numeric_limits<double>::infinity();
+	const double delta_x = v_1.x - v_2.x;
+	const double delta_y = v_1.y - v_2.y;
+	const double dist = sqrt(delta_x * delta_x + delta_y * delta_y);
+	return dist;
+}
+
+void SimpleGraph::reset_vertices() {
+	for (auto &v : vertices) v.deleted = false;
+}
+
+bool SimpleGraph::update_min_edge(const vector<Vertex>::iterator &v_1, const vector<Vertex>::iterator &v_2,
+	pair<vector<Vertex>::iterator, vector<Vertex>::iterator> &current_min) const {
+
+	const double potential_dist = dist(*v_1, *v_2);;
+	const double old_dist = dist(*current_min.first, *current_min.second);
+
+	// Replace current_min if potential distance < old distance.
+	if (potential_dist < old_dist) {
+		current_min = { v_1, v_2 };
+		return true;
+	}
+	return false;
+}
---
+++ SimpleGraph.h
@@ -0,0 +1,38 @@
+// Project Identifier: 5949F553E20B650AB0FB2266D3C0822B13D248B0
+#ifndef SIMPLEGRAPH_H
+#define SIMPLEGRAPH_H
+
+#include <iostream>
+#include <vector>
+
+class SimpleGraph {
+public:
+	SimpleGraph(std::istream &is, bool border_in);
+	size_t total_size() const;
+
+protected:
+	enum class VertType {LAND, WATER, COAST};
+
+	struct Vertex {
+		double running_dist;
+		int x, y;
+		size_t parent_index;
+		bool deleted;
+		VertType get_vert_type() const;
+		bool operator< (const struct Vertex &other);
+	}; // Vertex
+
+	std::vector<Vertex> vertices;
+	const bool border;
+
+	double dist(const Vertex &v_1, const Vertex &v_2) const;
+	size_t working_size() const;
+	void reset_vertices();
+	static bool is_valid_vertex(const Vertex &v);
+
+	bool update_min_edge(const std::vector<Vertex>::iterator &v_1, const std::vector<Vertex>::iterator &v_2,
+		std::pair<std::vector<Vertex>::iterator, std::vector<Vertex>::iterator> &current_min) const;
+
+}; // SimpleGraph
+
+#endif
